2821b94297e9b02e56630bd98d4ce59cd1831668
Gleb Gorelov <gorelov@haulmont.com>
2019-08-20 16:41:16 +0400

    Update to platform-7.0.10

--- modules/web-toolkit/src/com/company/demo/web/toolkit/ui/client/lookupfield/LookupFieldExtensionConnector.java
+++ modules/web-toolkit/src/com/company/demo/web/toolkit/ui/client/lookupfield/LookupFieldExtensionConnector.java
@@ -3,7 +3,7 @@ package com.company.demo.web.toolkit.ui.client.lookupfield;
 import com.company.demo.web.toolkit.ui.lookupfield.LookupFieldExtension;
 import com.vaadin.client.ServerConnector;
 import com.vaadin.client.extensions.AbstractExtensionConnector;
-import com.vaadin.client.ui.VFilterSelect;
+import com.vaadin.client.ui.VComboBox;
 import com.vaadin.client.ui.combobox.ComboBoxConnector;
 import com.vaadin.shared.ui.Connect;
 
@@ -11,9 +11,10 @@ import com.vaadin.shared.ui.Connect;
 public class LookupFieldExtensionConnector extends AbstractExtensionConnector {
     @Override
     protected void extend(ServerConnector target) {
-        final VFilterSelect combobox = ((ComboBoxConnector) target).getWidget();
+        final VComboBox combobox = ((ComboBoxConnector) target).getWidget();
 
-        registerRpc(LookupFieldExtensionClientRpc.class,
-                (LookupFieldExtensionClientRpc) combobox::applyNewSuggestions);
+        registerRpc(LookupFieldExtensionClientRpc.class, (LookupFieldExtensionClientRpc) () ->
+                combobox.suggestionPopup.showSuggestions(combobox.currentPage)
+        );
     }
 }
